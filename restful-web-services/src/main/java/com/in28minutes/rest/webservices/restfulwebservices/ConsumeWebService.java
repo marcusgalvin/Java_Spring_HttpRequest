c8e202ca6b2a2f233782e7320102452632986b67
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2020-05-26 12:59:46 -0400

    JSON data parse success

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/ConsumeWebService.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/ConsumeWebService.java
@@ -1,11 +1,18 @@
 package com.in28minutes.rest.webservices.restfulwebservices;
 
 import java.awt.PageAttributes.MediaType;
+import java.net.URI;
+import java.net.http.HttpClient;
 import java.net.http.HttpHeaders;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Arrays;
+import java.util.Map;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JsonParserFactory;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.ui.Model;
@@ -14,6 +21,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.core.JsonParser;
 
 @RestController
 public class ConsumeWebService {
@@ -28,23 +37,40 @@ public class ConsumeWebService {
 //	  }
 	
 	
-	@GetMapping(path="/players")
-	private static String getEmployees(String Name)
-	{
+	
+
+	
+@GetMapping(path="/players")
+public static void getPlayers() {
 		
-//		public String Name;
-		
-	    final String uri = "https://api.sportsdata.io/v3/nfl/scores/json/Teams?key=014ecc9bac8a4a5d94b6625f3fdcf910";
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder()
+				.uri(URI.create("https://api.sportsdata.io/v3/nfl/scores/json/Teams?key=014ecc9bac8a4a5d94b6625f3fdcf910")).build();
+		client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+				.thenApply(HttpResponse::body)
+				.thenApply(ConsumeWebService::parse)
+				.join();		
 
-	    RestTemplate restTemplate = new RestTemplate();
-	    String teams = restTemplate.getForObject(uri, String.class);
-	    
-	    return "Team: " + Name;	    
-
-
-//	    System.out.println(teams);
-	   
 	}
+	
+	public static String parse(String responseBody) {
+		JSONArray teams = new JSONArray(responseBody);
+		
+		for(int i = 0; i < teams.length(); i++) {
+			JSONObject team = teams.getJSONObject(i);
+			int id = team.getInt("TeamID");
+			String name = team.getString("Name");
+			String city = team.getString("City");
+			String division = team.getString("Conference");
+			
+			System.out.println("Team Id: " + id + '\n' + city + " " + name + '\n' + "Conference: " + division);
+			System.out.println("--------------------");
+		}
+		return responseBody;
+		
+	}
+	
+	
 	
 	
 	
