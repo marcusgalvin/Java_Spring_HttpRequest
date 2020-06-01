84419383f82940a12d02a2e224a41b651b4e4953
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2020-05-31 22:22:50 -0400

    added react

--- restful-web-services/client/src/App.js
+++ restful-web-services/client/src/App.js
@@ -1,39 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-class App extends Component {
-  state = {
-    isLoading: false,
-    greeting: ""
-  };
-  sayHello = async (event) => {
-    event.preventDefault();
-    let response = await fetch('/greeting?name=' + this.state.greeting);
-    let body = await response.json();
-    this.setState({ greeting: body.name, isLoading: false, isGreetingVisible: '' });
-  }
-  updateName = (event) => {
-    event.preventDefault();
-    this.setState({greeting: event.target.value, isLoading: false});
-  }
-  render() {
-    const {greeting, isLoading} = this.state;
-    if (isLoading) {
-    return 
+import login from './components/login/login';
 
-    }
+class App extends Component {
+  render() {
     return (
-<div className="App">
-<header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-<div className="App-intro">
-            <input onChange={(event)=>this.updateName(event)} placeholder="Enter Your Name"></input>
-            <button onClick={(event)=>this.sayHello(event)}>Please Click Me!</button>
-<h2 style={{visibility: this.isGreetingVisible}}>Hello {this.state.greeting}</h2>
-        </div>
-        </header>
-    </div>
+      <div className="App">
+        My hello world hi
+      <login />
+      </div>
     );
   }
 }
+
+
+
 export default App;
---
+++ restful-web-services/client/src/login/login.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class login extends Component{
+	redner(){
+		return (
+			<div className="login">
+			My login page
+			</div>
+		)
+	}
+}
+
+export default login;
