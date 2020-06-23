9a0726533624e5b069f7217a48afa12c45c73d35
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2020-06-23 17:58:37 -0400

    added more variables to my rank algorithm

--- restful-web-services/client/src/components/allPlayers/PlayerCompare.jsx
+++ restful-web-services/client/src/components/allPlayers/PlayerCompare.jsx
@@ -35,144 +35,173 @@ for(var i = 0; i < data.length; i++){
   var sacks = data[i].Sacks;
   var defensiveTouchdowns = data[i].DefensiveTouchdowns;
   var tacklesForLoss = data[i].TacklesForLoss;
+  var interceptions = data[i].Interceptions;
+  var forcedFumbles = data[i].FumblesForced;
+  var fumbleReturnYards = data[i].FumbleReturnYards;
 
-  console.log(team + " scored " + data[i].FantasyPoints + " fantasy points");
 
-  var allPoints = 5402.8
-  // console.log(totalPoints)
+ 
 
-  var eachTeamFantasyPointPercentage = data[i].FantasyPoints / allPoints
-  // var eachTeamSacks = data[i].Sacks
-
-  // console.log(data[i].Team + "'s percent is " + eachTeamFantasyPointPercentage);
-
-  //test
+  //make weight = to blocked kicks and track it as a data point from the API
+  //set weight to 0
   weight = data[i].BlockedKicks - data[i].BlockedKicks;
-  console.log("Sacks: " + sacks)
-  // console.log(Math.max(data[i].Sacks))
 
+//fumble return yards
+var highestLeagueFumbleReturnYards = 256;
+var fumbleReturnYardsPercentage = (fumbleReturnYards/ highestLeagueFumbleReturnYards) * 100;
+console.log("here" + fumbleReturnYards)
 
-
-//weigh for fantasy points
-if(eachTeamFantasyPointPercentage >= 0.075){
-  console.log(team)
-  console.log(team + "is in the top 75%")
-
-} else if(eachTeamFantasyPointPercentage >= .050 && eachTeamFantasyPointPercentage <= 0.075){
-
-  console.log(team + " has between .50 and .75%")
-
-  weight =  0.5;
-
-  console.log(team + " weight after calc FantasyPts " + weight)
-
-
-} else if(eachTeamFantasyPointPercentage <= .050 && eachTeamFantasyPointPercentage>= .025){
-  console.log(team + " has between .25 and .50%")
-
-    weight = 0.25;
-      console.log(team + " weight after calc FantasyPts " + weight)
-
-} else if(eachTeamFantasyPointPercentage <= .025){
-  console.log(team + " has less than .25%")
-
-      weight = 0.1;
-        console.log(team + " weight after calc FantasyPts " + weight)
-
-
+if(fumbleReturnYardsPercentage > 75){
+  console.log(team + " fumble return yards " + fumbleReturnYards)
+  weight = weight + 0.75;
+  console.log("weight after fumble return yard calc " + weight)
+} else if(fumbleReturnYardsPercentage < 75 && fumbleReturnYardsPercentage <= 50){
+  console.log(team + " fumble return yards " + fumbleReturnYards)
+  weight = weight + 0.50;
+  console.log("weight after fumble return yard calc " + weight)
+}else if (fumbleReturnYardsPercentage < 50 && fumbleReturnYardsPercentage >= 25){
+  console.log(team + " fumble return yards " + fumbleReturnYards)
+  weight = weight + 0.25;
+  console.log("weight after fumble return yard calc " + weight)
+} else if (fumbleReturnYardsPercentage < 25){
+  console.log(team + " fumble return yards " + fumbleReturnYards)
+  weight = weight + 0.1;
+  console.log("weight after fumble return yard calc " + weight)
+}else if (fumbleReturnYardsPercentage == 0){
+  console.log(team + " fumble return yards " + fumbleReturnYards)
+  weight = weight + 0.0;
+  console.log("weight after fumble return yard calc " + weight)
+}else if (fumbleReturnYardsPercentage < 0){
+  console.log(team + " fumble return yards " + fumbleReturnYards)
+  weight = weight - 0.25;
+  console.log("weight after fumble return yard calc " + weight)
 }
 
 
 
-//weight test #2 - sacks
-if(sacks >= 75){
-  console.log(team + " has " + sacks + " sacks");
+//forced fumbles percentage calc
+var highestLeagueForcedFumbles = 35;
+var forcedFumblesPercentage = (forcedFumbles / highestLeagueForcedFumbles) * 100;
+
+if(forcedFumblesPercentage > 75){
+  console.log(team + " forced fumbles " + forcedFumbles)
   weight = weight + 0.75;
-  console.log("weight after calc sacks " + weight);
-
-
-} else if (sacks >=50 && sacks < 75){
-  console.log(team + " has " + sacks +" sacks");
-  weight = weight + .50;
-  console.log("weight after calc sacks " + weight);
-
-
-}else if (sacks >25 && sacks < 50){
-  console.log(team + " has " + sacks +" sacks");
-  weight = weight + .25;
-  console.log("weight after calc sacks " + weight);
-
-
-} else if (sacks < 25){
-  console.log(team + " has " + sacks +" sacks");
-  weight = weight + .1;
-  console.log("weight after calc sacks " + weight);
-
+  console.log("weight after forced fumble calc " + weight);
+}else if (forcedFumblesPercentage < 75 && forcedFumbles >= 50){
+  console.log(team + " forced fumbles " + forcedFumbles)
+  weight = weight + 0.50;
+  console.log("weight after forced fumble calc " + weight);
+}else if(forcedFumblesPercentage < 50 && forcedFumbles >= 25){
+  console.log(team + " forced fumbles " + forcedFumbles)
+  weight = weight + 0.25;
+  console.log("weight after forced fumble calc " + weight);
+} else if(forcedFumblesPercentage < 25){
+  console.log(team + " forced fumbles " + forcedFumbles)
+  weight = weight + 0.1;
+  console.log("weight after forced fumble calc " + weight);
 }
 
 
-//weight test #3 - tackles for loss
-//average league tackles for loss = 132.73
-if(tacklesForLoss > 150){
-  console.log(team + " has " + tacklesForLoss + " tackles for loss")
-  weight = weight + 1;
-  console.log("weight after tackles for loss calc " + weight)
 
-} else if(tacklesForLoss < 150 && tacklesForLoss >=100) {
+//interceptions percentage calc
+var highestLeagueInterception = 38;
+var interceptionPercentage = (interceptions / highestLeagueInterception) * 100;
+
+if(interceptionPercentage > 75){
+    console.log(team + " has " + interceptions + "interceptions");
+    weight = weight + 0.75;
+    console.log("weight after interpcetion calc " + weight);
+} else if(interceptionPercentage < 75 && interceptionPercentage >= 50){
+    console.log(team + " has " + interceptions + "interceptions");
+    weight = weight + 0.50;
+    console.log("weight after interpcetion calc " + weight);
+} else if(interceptionPercentage < 50 && interceptionPercentage >= 25){
+    console.log(team + " has " + interceptions + "interceptions");
+    weight = weight + 0.25;
+    console.log("weight after interpcetion calc " + weight);
+} else if(interceptionPercentage < 25){
+    console.log(team + " has " + interceptions + "interceptions");
+    weight = weight + 0.1;
+    console.log("weight after interpcetion calc " + weight);
+}
+
+
+
+
+
+
+//sack percentage calc
+
+var highestLeagueSackNumber = 81;
+var sackPercentage = (sacks / highestLeagueSackNumber) * 100;
+
+if(sackPercentage  >= 75){
+    console.log(team + " has " + sacks + " sacks");
+    weight = weight + 0.75;
+    console.log("weight after calc sacks " + weight);
+} else if (sackPercentage < 75 && sackPercentage >= 50){
+    console.log(team + " has " + sacks + " sacks");
+    weight = weight + 0.50;
+    console.log("weight after calc sacks " + weight);
+} else if(sackPercentage < 50 && sackPercentage >= 25){
+    console.log(team + " has " + sacks + " sacks");
+    weight = weight + 0.25;
+    console.log("weight after calc sacks " + weight);
+} else if(sackPercentage < 25){
+    console.log(team + " has " + sacks + " sacks");
+    weight = weight + 0.1;
+    console.log("weight after calc sacks " + weight);
+}
+
+
+//tackles for loss percentage calc
+var highestleagueTacklesForLoss = 175;
+var tacklesForLossPercentage = (tacklesForLoss / highestleagueTacklesForLoss) * 100;
+
+if(tacklesForLossPercentage > 75){
   console.log(team + " has " + tacklesForLoss + " tackles for loss")
   weight = weight + 0.75;
   console.log("weight after tackles for loss calc " + weight)
-
-} else if (tacklesForLoss < 100 && tacklesForLoss >= 75){
+} else if (tacklesForLossPercentage < 75 && tacklesForLossPercentage >= 50){
   console.log(team + " has " + tacklesForLoss + " tackles for loss")
   weight = weight + 0.50;
   console.log("weight after tackles for loss calc " + weight)
-
-} else if (tacklesForLoss < 75 && tacklesForLoss >= 50){
+} else if (tacklesForLossPercentage < 50 && tacklesForLossPercentage >= 25){
   console.log(team + " has " + tacklesForLoss + " tackles for loss")
   weight = weight + 0.25;
   console.log("weight after tackles for loss calc " + weight)
-
-} else if( tacklesForLoss < 50){
+} else if (tacklesForLossPercentage < 25){
   console.log(team + " has " + tacklesForLoss + " tackles for loss")
   weight = weight + 0.1;
   console.log("weight after tackles for loss calc " + weight)
-
 }
 
 
+//defenvie touchdown percentage calc
+var highestLeagueDefensiveTds = 14;
+var defensiveTdPercentage = (defensiveTouchdowns / highestLeagueDefensiveTds) * 100;
 
-
-
-
-
-//weight test #3 - touchdowns
-if(defensiveTouchdowns > 10){
-  console.log(team + " has " + defensiveTouchdowns + " defensive TDs");
-  weight = weight + 1.0;
-  console.log("weight after DEF TD calc " + weight)
-
-
-} else if(defensiveTouchdowns < 10 && defensiveTouchdowns >=7){
+if(defensiveTdPercentage > 75){
   console.log(team + " has " + defensiveTouchdowns + " defensive TDs");
   weight = weight + 0.75;
   console.log("weight after DEF TD calc " + weight)
-
-} else if(defensiveTouchdowns < 7 && defensiveTouchdowns >=3){
+} else if (defensiveTdPercentage < 75 && defensiveTdPercentage >= 50){
   console.log(team + " has " + defensiveTouchdowns + " defensive TDs");
-  weight = weight + 0.5;
+  weight = weight + 0.50;
   console.log("weight after DEF TD calc " + weight)
-
-} else if(defensiveTouchdowns < 3 && defensiveTouchdowns > 0){
+} else if (defensiveTdPercentage < 50 && defensiveTdPercentage >= 25){
   console.log(team + " has " + defensiveTouchdowns + " defensive TDs");
   weight = weight + 0.25;
   console.log("weight after DEF TD calc " + weight)
-
-} else if(defensiveTouchdowns == 0){
+} else if (defensiveTdPercentage < 25){
   console.log(team + " has " + defensiveTouchdowns + " defensive TDs");
-  weight = weight + 0;
+  weight = weight + 0.1;
   console.log("weight after DEF TD calc " + weight)
 }
+
+
+
+
 
 console.log(weight);
   console.log("-----------------------")
@@ -190,6 +219,7 @@ console.log(weight);
 //print total league points to compare each team too
   console.log("total league fantasy DEF league points " + totalPoints);
   console.log("total league tackles for loss " + totalLeagueTacklesForLoss);
+
   console.log("total league tackles for loss " + totalLeagueTacklesForLoss / 32);
 
 
